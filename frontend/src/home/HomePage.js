f69cfd1c220da766924409f5b9bb8749a417ad28
Sayantan <sayantan.kundu95@gmail.com>
2020-08-04 14:11:19 +0530

    Bug fixes

--- frontend/src/home/HomePage.js
+++ frontend/src/home/HomePage.js
@@ -24,7 +24,7 @@ const HomePage = (props) => {
         setPassword(event.target.value);
     }
 
-    
+
 
     const loginClick = () => {
         let userRole = document.getElementById('role').value;
@@ -45,41 +45,44 @@ const HomePage = (props) => {
 
     }
     return (
-        <div className="main-container">
-            <form>
-                <p>
-                    <label htmlFor="role">Choose a role:</label>
-                    <select id="role" name="role" onChange={handleRoleChange}>
-                        <option value="user">User</option>
-                        <option value="admin">Admin</option>
-                    </select>
-                </p>
-
-                <p>
-                    <label htmlFor="email">Username: </label>
-                    <input type="text" id="email" onKeyUp={handleEmailChange}></input>
-                </p>
-
-                {role === 'admin' ? <React.Fragment>
+        <div>
+            <div style={{ textAlign: 'center' }}>
+                <h1>Welcome to National Library</h1>
+            </div>
+            <div className="home-container">
+                <form>
                     <p>
-                        <label htmlFor="email">Password: </label>
-                        <input type="password" onKeyUp={handlePasswordChange}></input>
+                        <label htmlFor="role">Choose a role:</label>
+                        <select id="role" name="role" onChange={handleRoleChange}>
+                            <option value="user">User</option>
+                            <option value="admin">Admin</option>
+                        </select>
                     </p>
-                </React.Fragment> : <br />
-                }
 
-                <p>
-                    <button type="button" onClick={loginClick}>Login</button>
-                    <div style={{ paddingTop: '20px' }}>
-                        <span>Not registered yet?</span>
-                        <NavLink style={{ paddingLeft: '20px' }} to="/register">Register Here</NavLink>
-                    </div>
-                </p>
-            </form>
+                    <p>
+                        <label htmlFor="email">Username: </label>
+                        <input type="text" id="email" onKeyUp={handleEmailChange}></input>
+                    </p>
 
-            <br />
+                    {role === 'admin' ? <React.Fragment>
+                        <p>
+                            <label htmlFor="email">Password: </label>
+                            <input type="password" onKeyUp={handlePasswordChange}></input>
+                        </p>
+                    </React.Fragment> : <br />
+                    }
 
+                    <p>
+                        <button type="button" onClick={loginClick}>Login</button>
+                        <div style={{ paddingTop: '20px' }}>
+                            <span>Not registered yet?</span>
+                            <NavLink style={{ paddingLeft: '20px' }} to="/register">Register Here</NavLink>
+                        </div>
+                    </p>
+                </form>
 
+                <br />
+            </div>
         </div>
     )
 }

10b10637d161edd748eb566d05508f31150ff420
Sayantan <sayantan.kundu95@gmail.com>
2020-08-09 17:37:24 +0530

    Implement swagger doc for APIs

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { graphqlHTTP } = require('express-graphql')
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerui = require('swagger-ui-express');
+const { graphqlHTTP } = require('express-graphql');
+
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const graphqlSchema = require('./graphql/schema');
@@ -22,10 +25,32 @@ app.use((req, res, next) => {
     next();
 })
 
+//swagger config
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Library API Docs",
+            description: "Description for Library Management System APIs",
+            contact: {
+                name: "Library "
+            },
+            servers: "http://localhost:4001/",
+        }
+    },
+    apis: ["./routes/userRoutes.js"]
+}
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerDocs));
+
+//end of swagger config
+
+/**
+ * @swagger
+ */
 app.use('/api/user', userRoutes);
 app.use('/api/admin', adminRoutes);
 
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,21 @@ const userController = require('../controllers/userController');
 
 const userRoutes = express.Router();
 
+/**
+ * 
+ * @swagger
+ * /api/user/books:
+ *  get:
+ *      description: Get all books
+ *      responses:
+ *          '200':
+ *              description: A succesful response
+ *          '500':
+ *              description: Internal server error
+ *          '404':
+ *              description: Book not found 
+ * 
+ */
 userRoutes.get('/books', userController.getBooks);
 
 userRoutes.post('/adduser', userController.addUser);
